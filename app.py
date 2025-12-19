f3bd21be6c706e78255f39ec28dbd1a3d29b6939
Adnan halde <adnanhalde8899@gmail.com>
2025-12-20 00:27:52 +0530

    Implement manual encoding for input features
    Replaced encoder loading with manual encoding for inputs.

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-import os
 import streamlit as st
 import numpy as np
 import joblib
 
-st.write("Current directory:", os.getcwd())
-st.write("Files in directory:", os.listdir())
-
 model = joblib.load("satellite_launch_cost_model.pkl")
-encoders = joblib.load("encoders.pkl")
-
 
 st.title("🚀 Satellite Launch Cost Prediction")
 st.write("Estimate the cost of launching a satellite (in Million USD)")
@@ -20,10 +14,14 @@ orbit = st.selectbox("Orbit Type", ["LEO", "MEO", "GEO"])
 rocket = st.selectbox("Rocket Class", ["Light", "Medium", "Heavy"])
 fuel = st.selectbox("Fuel Type", ["Solid", "Liquid", "Cryogenic"])
 
-# Encode inputs
-orbit_enc = encoders["orbit_type"].transform([orbit])[0]
-rocket_enc = encoders["rocket_class"].transform([rocket])[0]
-fuel_enc = encoders["fuel_type"].transform([fuel])[0]
+# Manual encoding
+orbit_map = {"LEO": 0, "MEO": 1, "GEO": 2}
+rocket_map = {"Light": 0, "Medium": 1, "Heavy": 2}
+fuel_map = {"Solid": 0, "Liquid": 1, "Cryogenic": 2}
+
+orbit_enc = orbit_map[orbit]
+rocket_enc = rocket_map[rocket]
+fuel_enc = fuel_map[fuel]
 
 if st.button("Predict Launch Cost"):
     X = np.array([[payload_weight, orbit_enc, rocket_enc, fuel_enc, mission_years]])
